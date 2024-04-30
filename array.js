14f057d7a9ef3634c557edda15e19609877a3b05
kaho0 <kahonbintezaman@gmail.com>
2024-04-30 20:23:42 +0600

    reverse

---
+++ array.js
@@ -0,0 +1,11 @@
+var numbers=[10,20,30,40,50,60,70,80];
+for(var i=0;i<numbers.length;i++)
+{ var number =numbers[i];
+    console.log(number);
+}
+var items=['bottle','mouse','sunglass','pen'];
+for(var i=0;i<items.length;i++)
+{
+    var item =items[i];
+    console.log(item);
+}
