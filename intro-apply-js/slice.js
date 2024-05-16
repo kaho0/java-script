ac9b26b4412b3a958c768f33af564132a977f47a
kaho0 <kahonbintezaman@gmail.com>
2024-05-16 22:50:22 +0600

    duplicate

---
+++ intro-apply-js/slice.js
@@ -0,0 +1,6 @@
+const friends=[12,3,4,5,66,5,4,33];
+const partial=friends.slice(3,6);
+const paarrtiall=friends.splice(3,6,99,55,111);
+console.log(partial);
+console.log(paarrtiall);
+console.log(friends);
