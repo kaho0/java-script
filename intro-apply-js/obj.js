7d8e17ab6d4da836b3c69193142fb9388e27ede5
kaho0 <kahonbintezaman@gmail.com>
2024-06-07 10:33:03 +0600

    argu

---
+++ intro-apply-js/obj.js
@@ -0,0 +1,20 @@
+const student ={
+    name:"Kahon binte zaman",
+    id:"BKH2301040F",
+    adress:"Chowrasta,Apon nibash",
+    isSingle:true,
+    friends:['ashra','rodela','arthy','moinnnnaa','abira'],
+    car:{
+        brand:'tesla',
+        price:120000000,
+        made:2027,
+        manufacturer:{
+            name:'teslaa',
+            CEO:"elon mask",
+            country:"USA",
+        }
+    }
+
+}
+console.log(student.car);
+console.log(student);
