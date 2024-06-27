c49213d1dc7b9b8decd83925147ca7d002f63ae8
kahon <kahonbintezaman@gmail.com>
2024-06-27 20:48:07 +0600

    javas

---
+++ data.js
@@ -0,0 +1,2 @@
+const data = [{ id: 1, name: 'abul', address: 'kochukhet' }];
+console.log(data[0].address); // This will log: 'kochukhet'
--- inject.js
+++ inject.js
@@ -1,19 +1,24 @@
-const a=56;
-const num=[4,5,6,7,8];
-const person={
-    name:'sakib khan';
+const a = 56;
+        const num = [4, 5, 6, 7, 8];
+        const person = {
+            name: 'sakib khan'
+        };
 
-}
-const message=`hi, ${person.name} has a : ${a} access to ${num[1]}`
+        const message = `hi, ${person.name} has a : ${a} access to ${num[1]}`;
+        console.log(message); // This will log: "hi, sakib khan has a : 56 access to 5"
 
-const square=x=>x*x;
-const sum=(a,b) => a + b;
-const {age,z,...others}={x:2,y:5,z:3,name:'kuttaa',age:44};
+        const square = x => x * x;
+        const sum = (a, b) => a + b;
 
+        const { age, z, ...others } = { x: 2, y: 5, z: 3, name: 'kuttaa', age: 44 };
+        console.log(age); // This will log: 44
+        console.log(z); // This will log: 3
+        console.log(others); // This will log: { x: 2, y: 5, name: 'kuttaa' }
 
-const [first,second,...remaining]=['ram','sham','jodu','modu'];
+        const [first, second, ...remaining] = ['ram', 'sham', 'jodu', 'modu'];
+        console.log(first); // This will log: 'ram'
+        console.log(second); // This will log: 'sham'
+        console.log(remaining); // This will log: ['jodu', 'modu']
 
-
-//for of used in string
-//for in used in object            
-
+        const data = [{ id: 1, name: 'abul', address: 'kochukhet' }];
+        console.log(data[0].address); // This will log: 'kochukhet'
