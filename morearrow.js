bdf0ba2a46ec3c89f7a68a58d2040a01be205a12
kahon <kahonbintezaman@gmail.com>
2024-06-28 23:34:37 +0600

    objects

---
+++ morearrow.js
@@ -0,0 +1,3 @@
+const getThird = numbers =>numbers[2];
+const third=getThird([2,3,4,6,7,8])
+console.log(third);
---
+++ templatestring.js
@@ -0,0 +1,10 @@
+function fullName(first,last=''){
+    const full=first+' '+last;
+    return full;
+}
+function friends(numbs=[]){
+
+}
+const a=5,b=2;
+const math=`the sum of ${a} and ${b} is ${a+b}`
+console.log(math);
