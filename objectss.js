ffb094312f07d8e3bbef8b5a4fe815ac2cafb490
kahon <kahonbintezaman@gmail.com>
2024-06-30 23:51:52 +0600

    oops

---
+++ objectss.js
@@ -0,0 +1,10 @@
+class product{
+    country='bangladesh';
+    speak(talk){
+        console.log(`talking about ${talk}`)
+    }
+}
+
+const lenovo=new product();
+console.log(lenovo);
+lenovo.speak('obahh kiya kow');
